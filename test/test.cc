6c188931ad1a42458c44fd28a9d5d50d962dc981
Mostafa Razavi <mostafa@sepent.com>
2013-09-29 15:50:58 +0330

    fixed a typo.

--- test/test.cc
+++ test/test.cc
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   QLOG_DEBUG("some debug information.");
   QLOG_INFO("some information.");
   QLOG_LOGIC("very logical indeed.");
-  QLOG_WARNING("your've been warned.");
+  QLOG_WARNING("you've been warned.");
   QLOG_ERROR("an error. run for your life!");
 
   return 0;
